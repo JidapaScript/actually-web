66f9839f791efd24019086b9862a894ce19a2cb9
JidapaScript <jidapasnidvongs@gmail.com>
2024-04-27 04:12:48 +0700

    additional setup page js css

---
+++ actually-web-main/components/TopBar.jsx
@@ -0,0 +1,9 @@
+export default function TopBar(){
+    return (
+        <top className="flex justify-between fixed bottom-0 w-full bg-black text-white p-8">
+            <div>profile</div>
+            <div>logo</div>
+            <div>search</div>
+        </top>
+    );
+})
