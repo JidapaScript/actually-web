64c85138de65ee82e91f4967665fb8870121f33e
JidapaScript <jidapasnidvongs@gmail.com>
2024-05-02 03:25:01 +0700

    crisis new package.json file cannot npm run dev

--- actually-web-main/components/TopBar.jsx
+++ actually-web-main/components/TopBar.jsx
@@ -1,9 +1,15 @@
 export default function TopBar(){
     return (
         <top className="flex justify-between fixed bottom-0 w-full bg-black text-white p-8">
-            <div>profile</div>
-            <div>logo</div>
-            <div>search</div>
+            <a href="#" className="profile"> 
+            <img className="profile-pic" src="profile_sm.png" alt="profile" /> 
+            </a>
+            <a href="#" className="actually-mainpage"> 
+            <img className="actually-logo" src="actually.svg" alt="actually" /> 
+            </a>
+            <a href="#" className="search-bar"> 
+            <img className="search" src="search.svg" alt="search" /> 
+            </a>
         </top>
     );
 }
---
+++ actually-web-main/src/app/main/layout.jsx
@@ -0,0 +1,10 @@
+import TopBar from "../../../components/TopBar";
+
+export default function MainLayout({children}){
+    return <div>
+        <h1>
+            {children}
+            <TopBar />
+        </h1>
+    </div>
+}
