c93ca233f61a2d86c7e4cd9116a349fb824e61de
JidapaScript <jidapasnidvongs@gmail.com>
2024-05-03 04:12:52 +0700

    more components while 404

---
+++ actually-web-main/src/app/components/CategoryTags.jsx
@@ -0,0 +1,13 @@
+import Tag from "@/app/components/Tag";
+
+export default function TopicCategories(){
+    return (
+        <Tag tagname="Favourite" />
+        <Tag tagname="Food" />
+        <Tag tagname="Science" />
+        <Tag tagname="Tech" />
+        <Tag tagname="Philosophy" />
+        <Tag tagname="Ethics" />
+        <Tag tagname="Global" />
+    )
+}
